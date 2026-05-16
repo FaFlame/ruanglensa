f4d9bb2c1438e9fa7910dea75af65fe64c70bf28
M. Aufa Rahman <aufarahman489@gmail.com>
2026-05-17 02:42:55 +0800

    major update bagian flow pemesanan pt2

--- lib/features/user/presentation/produk_service.dart
+++ lib/features/user/presentation/produk_service.dart
@@ -36,8 +36,6 @@ class ProdukService {
     final snapshot = await _db
         .collection('produk')
         .where('status_produk', isEqualTo: 'Tersedia')
-        .where('kategori_produk', whereNotIn: ['Aksesori'])
-        .orderBy('kategori_produk')
         .orderBy('created_at', descending: true)
         .limit(9)
         .get();
--- lib/features/user/presentation/struk_page.dart
+++ lib/features/user/presentation/struk_page.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import '../../user/presentation/user_page.dart';
+//import '../../user/presentation/status_sewa_page.dart';
 
 class StrukPage extends StatefulWidget {
   final String sewaId;
